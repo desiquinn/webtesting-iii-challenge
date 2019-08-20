025a769105f96c87e94b6b088aa4c2b2abc23bcc
Desiree Morris <desiquinncompany@gmail.com>
2019-08-20 15:24:19 -0400

    desiree-morris

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -8,14 +8,39 @@ import Controls from './Controls.js';
 
 describe('<Contols />', () => {
 
-    it('can not be opened or closed if locked', () => {
+    it('provide buttons to toggle the closed and locked states.', () => {
         
         const controls = rtl.render(<Controls/>);
+        controls.getByText(/lock gate/i);
+        controls.getByText(/close gate/i);
 
-        let lockButton = controls.getByText(/lock gate/i);
-        let openButton = controls.getByText(/close gate/i);
-
-        expect(openButton.textContent).toBe("Close Gate");
     });
     
+
+    it('the closed toggle button is disabled if the gate is locked', () => {
+        const state = {
+            locked: true,
+            closed: true
+        }
+        const controls = rtl.render(<Controls locked={state.locked} closed={state.closed}/>);
+        const closeButton = controls.getByText(/open gate/i);
+        const lockButton = controls.getByText(/unlock gate/i);
+        
+        expect(closeButton.disabled).toBeTruthy();
+        expect(lockButton.disabled).toBeFalsy();
+    });
+
+    it('the locked toggle button is disabled if the gate is open', () => {
+        const state = {
+            locked: false,
+            closed: false
+        }
+        const controls = rtl.render(<Controls locked={state.locked} closed={state.closed}/>);
+        const closeButton = controls.getByText(/close gate/i);
+        const lockButton = controls.getByText(/lock gate/i);
+        
+        expect(closeButton.disabled).toBeFalsy();
+        expect(lockButton.disabled).toBeTruthy();
+    });
+
 });
