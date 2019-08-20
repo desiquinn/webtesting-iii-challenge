3ff44b2fdfc21d7c4408d03eeee360a3a6a78c00
Desiree Morris <desiquinncompany@gmail.com>
2019-08-20 14:30:38 -0400

    comepleted the tests for display component

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -7,42 +7,87 @@ import Display from './Display.js';
 
 describe('<Display />', () => {
 
-    it('displays if gate is open/closed and if it is locked/unlocked', () => {
+    it('displays "Open" if gate is open', () => {
         
-        const display = rtl.render(<Display />);
+        const state = {
+            closed: false
+        }
+        const display = rtl.render(<Display closed={state.closed}/>);
+        const open = display.getByText(/open/i);
 
-
-
-    });
-    
-    it('displays "Closed" if the closed prop is true and "Open" if otherwise', () => {
-        
-        const display = rtl.render(<Display />);
-
+        expect(open.textContent).toBe("Open");
 
 
     });
 
-    it('displays "Locked" if the locked prop is true and "Unlocked" if otherwise', () => {
+    it('displays "Closed" if gate is closed', () => {
         
-        const display = rtl.render(<Display />);
+        const state = {
+            closed: true
+        }
+        const display = rtl.render(<Display closed={state.closed}/>);
+        const closed = display.getByText(/closed/i);
 
+        expect(closed.textContent).toBe("Closed");
+
+
+    });
+
+    it('displays "Locked" if gate is locked', () => {
+        
+        const state = {
+            locked: true
+        }
+        const display = rtl.render(<Display locked={state.locked}/>);
+        const locked = display.getByText(/locked/i);
+
+        expect(locked.textContent).toBe("Locked");
+
+
+    });
+
+    it('displays "Unlocked" if gate is unlocked', () => {
+        
+        const state = {
+            locked: false
+        }
+        const display = rtl.render(<Display locked={state.locked}/>);
+        const unlocked = display.getByText(/unlocked/i);
+
+        expect(unlocked.textContent).toBe("Unlocked");
 
 
     });
 
     it('when locked or closed use the red-led class', () => {
         
-        const display = rtl.render(<Display />);
+        const state = {
+            locked: true,
+            closed: true
+        }
+        const display = rtl.render(<Display locked={state.locked} closed={state.closed}/>);
+        const locked = display.getByText(/locked/i);
+        const closed = display.getByText(/closed/i);
 
+        expect(locked.classList[1]).toBe('red-led');
+        expect(closed.classList[1]).toBe('red-led');
 
 
     });
 
-    it('when unlocked or open use the green-led class', () => {
-        
-        const display = rtl.render(<Display />);
 
+    it('when unlocked or open use the green-led class', () => {
+
+        const state = {
+            locked: false,
+            closed: false
+        }
+        const display = rtl.render(<Display locked={state.locked} closed={state.closed}/>);
+        const unlocked = display.getByText(/unlocked/i);
+        const open = display.getByText(/open/i);
+
+        expect(unlocked.classList[1]).toBe('green-led');
+        expect(open.classList[1]).toBe('green-led');
 
 
     });
