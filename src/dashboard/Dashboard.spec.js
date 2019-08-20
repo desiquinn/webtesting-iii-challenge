0ebf8e0594f6111045307fbe9fb39be03caa6d37
Desiree Morris <desiquinncompany@gmail.com>
2019-08-20 11:13:50 -0400

    installed correct devDependencies for testing

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,33 @@
 // Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Dashboard from './Dashboard.js';
+
+
+describe('<Dashboard />', () => {
+
+    it('should default to unlocked', () => {
+
+        const lockedValue= false;
+
+        expect(Dashboard.state.locked).toBe(lockedValue)
+
+    });
+
+    it('should default to closed', () => {
+
+        const closedValue= false;
+
+        expect(Dashboard.state.closed).toBe(closedValue)
+        
+    })
+
+    it('can not be opened or closed if locked', () => {
+        
+        if(Dashboard.state.locked === true) {
+            expect(Dashboard.toggleClosed).not.toBeCalled()
+        }
+
+    });
+});
